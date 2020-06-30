948366467c2f8702ee88e8f70567f2f3a49bfbea
Marcin Rogalski <marcin.rogalski@billennium.com>
2020-06-30 22:15:22 +0200

    undefined

--- publish.js
+++ publish.js
@@ -1,47 +1,59 @@
 const { exec, execSync } = require('child_process')
 const package = require('./package.json')
+const readline = require('readline');
 
-function parse( version ){
-	return version.trim().split( '.' )
+
+
+function parse(version) {
+	return version.trim().split('.')
 }
 
-function compare( version_1, version_2 ){
-	for( let segment = 0 ; segment < Math.max( version_1.length, version_2.length ) ; segment++ ){
-		if( version_1[ segment ] !== version_2[ segment ] ){
-			return version_1[ segment ] > version_2[ segment ]
+function compare(version_1, version_2) {
+	for (let segment = 0; segment < Math.max(version_1.length, version_2.length); segment++) {
+		if (version_1[segment] !== version_2[segment]) {
+			return version_1[segment] > version_2[segment]
 				? version_1
 				: version_2
 		}
 	}
 }
 
-async function publish(){
-	let output = await new Promise( ( resolve, reject ) => {
-		let child_process = exec('npm publish' )
+async function publish() {
+	let output = await new Promise((resolve, reject) => {
+		let commitMessage
+		let exitCode
 
-		child_process.on('exit', ( code ) => {
-			process.exit( code )
-		})
-	} )
+		let read = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+		read.question('Enter commit message:', (message) => {
+			commitMessage = message
+			rl.close()
+		});
+
+		exitCode = execSync(`git add * && git commit -m "${commitMessage}"`)
+		if( exitCode !== 0 ) process.exit( exitCode )
+		exitCode = execSync('npm publish')
+		process.exit(exitCode)
+	})
 
 	return output
 }
 
-async function start(){
+async function start() {
 
-	let version = parse( package.version )
+	let version = parse(package.version)
 	let remoteVersion
 
-	try{
-		remoteVersion = parse( execSync( `npm show ${ package.name } version`, { encoding: 'UTF-8' } ) )
+	try {
+		remoteVersion = parse(execSync(`npm show ${package.name} version`, { encoding: 'UTF-8' }))
 
-		let update = compare( version, remoteVersion ) === version
+		let update = compare(version, remoteVersion) === version
 
-		update || console.log( `Skipping due to confilcted versions. To publish increment project version over ${ remoteVersion.join('.') }.` )
+		update || console.log(`Skipping due to confilcted versions. To publish increment project version over ${remoteVersion.join('.')}.`)
 
 		!update || await publish()
-	} catch( error ){
-		process.exit( 1 )
+	} catch (error) {
+		process.exit(1)
 	}
 
 }
